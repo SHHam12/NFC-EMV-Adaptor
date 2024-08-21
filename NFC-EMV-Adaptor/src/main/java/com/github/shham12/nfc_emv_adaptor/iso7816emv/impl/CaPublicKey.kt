f04bd675be270eed865e6046c2c9a008ea04c30d
hsukhwa <hamsukhwa@gmail.com>
2024-08-21 16:20:20 -0400

    Modify CaPublicKey
    To prevent JSONArray parsing logic add @Expose annotation

--- NFC-EMV-Adaptor/src/main/java/com/github/shham12/nfc_emv_adaptor/iso7816emv/impl/CaPublicKey.kt
+++ NFC-EMV-Adaptor/src/main/java/com/github/shham12/nfc_emv_adaptor/iso7816emv/impl/CaPublicKey.kt
@@ -1,19 +1,20 @@
 package com.github.shham12.nfc_emv_adaptor.iso7816emv.impl
 
 import com.github.shham12.nfc_emv_adaptor.iso7816emv.PublicKeyCertificate
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class CaPublicKey(
-    @SerializedName("RID")
+    @Expose @SerializedName("RID")
     var rid: String,
 
-    @SerializedName("PKIndex")
+    @Expose @SerializedName("PKIndex")
     var index: String,
 
-    @SerializedName("Exponent")
+    @Expose @SerializedName("Exponent")
     override var exponent: String,
 
-    @SerializedName("Modulus")
+    @Expose @SerializedName("Modulus")
     override var modulus: String
 ) : PublicKeyCertificate {
 
